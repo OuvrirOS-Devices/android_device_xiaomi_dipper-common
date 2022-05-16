ec88a6def0c2104b0646575e5e5268e924831811
Levi Marvin <levimarvin@icloud.com>
2022-05-16 15:00:19 +0800

    repo init

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 OuvrirOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/dipper.mk
+
+COMMON_LUNCH_CHOICES := \
+    dipper-user \
+    dipper-userdebug \
+    dipper-eng
---
+++ dipper.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2022 OuvrirOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(LOCAL_DIR)/device.mk)
+$(call inherit-product, vendor/ouvrir/config/common.mk)
+
+# Product Configs
+## Name
+PRODUCT_NAME := dipper
+## Code
+PRODUCT_DEVICE := dipper
+## Brand
+PRODUCT_BRAND := Xiaomi
+## Model
+PRODUCT_MODEL := Mi 8
+## Pre-install programs
+PRODUCT_PACKAGES += \
+    Launcher3QuickStep \
+    WallpaperPicker
+## Language
+PRODUCT_LOCALES :=  zh_CN \
+	en_US
+## Manufacturer
+PRODUCT_MANUFACTURER := Xiaomi
+## System Name
+RODUCT_SYSTEM_NAME := dipper
+## GMS
+PRODUCT_USE_PREBUILT_GMS := no
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build Configs
+## Fingerprint
+BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/SP2A.220405.004/V9.5.5.0.OEAMIFA:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 SP2A.220405.004 V9.5.5.0.OEAMIFA release-keys" \
+    TARGET_PRODUCT="dipper"
